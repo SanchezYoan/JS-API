8fb0c02f6e0ea7d773b3bec66d4da2f0c1d9190b
SanchezYoan <yoan30470@hotmail.fr>
2023-05-17 12:41:05 +0200

    meal-app

--- cours/index.js
+++ cours/index.js
@@ -1,60 +1,139 @@
-// XMLHttpREquest => récupération de data
+// Définition
 
-// function reqListener() {
-//     console.log(this.responseText);
-// }
+// XMLHttpRequest
 
-// let req = new XMLHttpRequest();
-// req.onload = reqListener;
+function reqListener() {
+  // console.log(this.responseText);
+}
 
-// req.open("get", "data.txt", true);
+let req = new XMLHttpRequest();
+req.onload = reqListener;
 // req.open("get", "data.json", true);
-// req.open("get", "https://api.blablagues.net/?rub=blagues", true)
-// req.send();
+req.open("get", "data.txt", true);
+req.send();
 
-  // ------------ //
- //     Fetch    //
-// ------------ //
+// Fetch
 
-// then => promesse => asynchrone
-// fetch("mon-url", "objet d'option")
-//     .then((response) => {
-//     // console.log(response)
-// // catch => else
-// }).catch((err) => console.log(err))
+// fetch("url", "options").then(
+//   (response) => {
+//     // response
+//   }).catch((err) => console.log(err))
+// );
 
-// le paramètre de la fonction fléché = ce qui a été fait avant
 // fetch("data.txt")
-//  .then((res) => res.text()
-//  .then((data) => console.log(data)));
+//   .then((res) => res.text())
+//   .then((data) => console.log(data));
 
-// fetch("data.json").then((res) => res.json())
-// .then((data) => console.log(data));
+// fetch("data.json")
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
 
-// .then((data) => console.log(data))
+//---------------
+// Projet Blagues
+//---------------
 
-// fetch("data.json").then((res) => res.json());
+// L'interface Headers vous permet de créer vos propres objets d'en-têtes via le constructeur Headers
 
 const myHeaders = new Headers();
 
 const init = {
-    method: "GET",
-    headers: myHeaders,
-    mode: "cors",
+  method: "GET", // POST, PUT, DELETE
+  headers: myHeaders,
+  mode: "cors",
+  cache: "default",
+};
+fetch("data.json", init).then((res) => res.json());
+// .then((data) => console.log(data));
+
+//--------------------------
+// npm i -g json-server
+// json-server --w db.json
+
+let init2 = {
+  method: "POST", // POST, PUT, DELETE
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    pseudo: "From Scratch",
+    message: "Yo les gens !",
+  }),
+  mode: "cors",
+  credentials: "same-origin",
+};
+
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch("http://localhost:3000/posts", init2);
+  // for delete => id
+  // fetch("http://localhost:3000/posts/2", init2);
+});
+
+//-----------
+// Asycnhrone
+//-----------
+
+setTimeout(() => {
+  // console.log("Test !");
+}, 2000);
+
+// Promise
+// fetch('monlien').then(() => ...)
+
+// async/await => attend avant d'exec la suite
+async function fetchData() {
+  // await fetch('monlien')
+  // await maFonction();
+  // Execute ensuite...
 }
-const init2 = {
-    method: "POST",
-    headers: { "Content-Type": "application/json"},
-    body: JSON.stringify({
-      pseudo: "from Scratch",
-      message: "hello world !",
-    }),
-    mode: "cors",
-    credentials: "same-origin",
-}
+const fetchData2 = async () => {
+  // await fetch('monlien')
+  // await maFonction();
+  // Execute ensuite...
+};
 
-fetch("data.json", init2)
- .then((res) => console.log(res));
+//---------
+// User Api
+//---------
 
- // CRUD => Create (POST)? read (GET), update (PUT), Delete (DELETE)
+//--------------------------------
+// JSON
+// JSON = format de données
 
+// méthode .json() Elle retourne une promesse qui s'auto-résout en renvoyant le corps de la requête parsée au format JSON.
+
+// fetch("data.json")
+//   .then((res) => res.json())
+//   .then((data) => {
+//     // stringgify => convertie en Json
+//     let settings = JSON.stringify(data);
+//     // parse => transforme json en objet JS
+//     console.log(JSON.parse(settings)); 
+//   });
+
+//---------
+// Meal Api
+//---------
+
+//---------------------------------
+// Web API
+
+// Local storage
+localStorage.data = "je suis de la data"
+// document.body.textContent = localStorage
+// localStorage.removeItem("data")
+
+const objet = {
+  name: "Denis",
+  age: 22
+};
+
+// Il faut passer une chaine de caractere
+localStorage.user = JSON.stringify(objet);
+console.log(JSON.parse(localStorage.user));
+
+// Session storage
+sessionStorage.dataSettings = "55px";
+// console.log(sessionStorage.dataSettings);
+sessionStorage.clear();
+// Cookie + notif
